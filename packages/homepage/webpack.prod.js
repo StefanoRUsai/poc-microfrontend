1d9c404e671c3c8226d6aeb7b985d653e3cf57dd
Stefano Raimondo Usai <stefanoraimondousai@MBP-di-Stefano.home-life.hub>
2022-09-01 23:32:35 +0200

    add webpack.prod

---
+++ packages/homepage/webpack.prod.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { ModuleFederationPlugin } = require("webpack").container;
+const path = require("path");
+const deps = require("./package.json").dependencies;
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "production",
+  devServer: {
+    port: 3001,
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "homepage",
+      filename: "remoteEntry.js",
+      exposes: {
+        // expose each component
+        "./HomePage": "./src/components/HomePage/HomePage",
+      },
+      shared: {
+        ...deps,
+        react: { singleton: true, eager: true, requiredVersion: deps.react },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+        "react-router-dom":{
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-router-dom"],
+        },     
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
